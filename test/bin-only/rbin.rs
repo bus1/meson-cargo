1a3465c4e94b2046d6a3993c6ccf4515fc2862d1
David Rheinsberg <david@readahead.eu>
2023-07-10 10:41:04 +0200

    test: add bin-only test
    Add another test run that only builds a Rust binary, no other targets.
    Verify that the build is successful, and that the binary can be used.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

---
+++ test/bin-only/rbin.rs
@@ -0,0 +1,8 @@
+//! rbin - Rust Binary
+//!
+//! A simple Rust binary that returns 71 unconditionally. It has no
+//! dependencies and is as trivial as it gets.
+
+fn main() -> std::process::ExitCode {
+    71.into()
+}
